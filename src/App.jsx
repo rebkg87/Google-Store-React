0f68cacd26fff34ca2f797d416429cdfb64af7c4
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-06-12 12:58:08 +0200

    first button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.scss'
+import Button from './components/button/Button'
 
 function App() {
 
   return (
     <>
-     
+     <Button title="Add to cart"/>
     </>
   )
 }
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+function Button({title}) {
+    return(
+        <>
+        <button>{title}</button>
+        </>
+    )
+}
+
+Button.propTypes = {
+    title: PropTypes.string,
+}
+
+export default Button;
+
