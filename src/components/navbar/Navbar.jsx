071eca6a06a39a20f4ba2ae7ca8219e661a0016f
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-12 20:54:41 +0200

    Navbar appearence and position

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,35 @@
 import "./navbar.scss";
 
+
 const Navbar = () => {
     return (
     <header className="nav-wrapper"> 
-            <img className="nav-img" src="./images/images/google-logo.png" alt="logo-google" />
+            <img className="nav-img" src="/assets/images/google-logo.png"/>
             <nav className="menunav">
                 <ul className="menu">
-                    <li> <a href="">Phones</a></li>
-                    <li><a href="./index.html" class="active">Earbuds</a></li>
-                    <li><a href="./watches.html">Watches</a></li>
-                    <li><a href="">Smart Home</a></li>
-                    <li><a href="">Accessories</a></li>
-                    <li><a href="">Subsciptions</a></li>
+                    <li> <a>Phone</a></li>
+                    <li><a to="/"
+                    className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                    }
+                            > Earbuds
+                        </a></li>
+                    <li> <a to="/"
+                    className={({ isActive, isPending }) => 
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }
+                            > Watches
+                        </a></li>
+                    <li><a>Smart Home</a></li>
+                    <li><a>Accessories</a></li>
+                    <li><a>Subsciptions</a></li>
                 </ul>
             </nav>
             <span class="iconos-nav">
-                <img src="./images/images/icons/Search.svg" alt="Search" />
-                <img src="./images/images/icons/Help.svg" alt="Help" />
-                <img src="./images/images/icons/Cart.svg" alt="Cart" />
-                <img src="./images/images/icons/Avatar.svg" alt="Avatar" />
+                <img src="/assets/images/icons/Search.svg"  />
+                <img src="/assets/images/icons/Help.svg"  />
+                <img className="cart-icon" src="/assets/images/icons/Cart.svg" />
+                <img src="/assets/images/icons/Avatar.svg" />
             </span>
         </header>
 
