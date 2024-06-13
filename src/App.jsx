eae834c16cff9e1d87fcb7b5beb73a2d9a9f768f
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-13 17:28:23 +0200

    App styles update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.scss";
+import WatchIndex from "./components/indexWatch/IndexWatch";
 
 
 function App() {
 
   return (
     <>
-
-
+     <WatchIndex />
     </>
   )
 }
--- src/components/indexWatch/IndexWatch.jsx
+++ src/components/indexWatch/IndexWatch.jsx
@@ -1,14 +1,33 @@
-import "./paybox.scss";
+import "./indexWatch.scss";
 import Navbar from "../navbar/Navbar";
 import Paybox from "../paybox/Paybox";
+import { WatchMainPhoto } from "../photos/MainPhoto";
+import ProductInformation from "../product-information/ProductInformation";
+import { WatchSelectColors } from "../photos/Watch/WatchSelectColors";
 
 function WatchIndex() {
-    <>
-        <Navbar />
-        <Paybox />
-    
-    </>
 
-}
+    return (
+        <body>
+            <Navbar />
+            <main className="main">
+                <div className="arrow">
+                    <a href=""><img src="./assets/images/icons/Arrow.svg" class="flecha"/></a> <p className="see-product">See product details</p>
+                </div>
+                <section className="product-details">
+                    <section className="left">
+                        <WatchMainPhoto />
+                    </section>
+                    <section className="right">
+                        <ProductInformation />
+
+                        <WatchSelectColors />
+                        <Paybox className="paybox"/>
+                    </section>
+                </section>
+            </main>
+        </body>
+    )
+};
 
 export default WatchIndex;
