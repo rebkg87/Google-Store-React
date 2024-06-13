c3b34cbe915a7711ff8030448fab652a5033bdbd
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-13 18:37:07 +0200

    Installation routes react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.scss";
 import WatchIndex from "./components/indexWatch/IndexWatch";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
 
   return (
-    <>
-     <WatchIndex />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WatchIndex />} />
+        <Route path="/" element={< IndexEarbuds/>} />
+        <Route path="/" element={< CartPage />} />
+      </Routes>
+    </Router>
   )
 }
 
