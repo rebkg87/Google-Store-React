fa3b964c822be19962fcb371b25c444b83efb553
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-13 14:31:45 +0200

    Index created

---
+++ src/components/indexWatch/IndexWatch.jsx
@@ -0,0 +1,14 @@
+import "./paybox.scss";
+import Navbar from "../navbar/Navbar";
+import Paybox from "../paybox/Paybox";
+
+function WatchIndex() {
+    <>
+        <Navbar />
+        <Paybox />
+    
+    </>
+
+}
+
+export default WatchIndex;
