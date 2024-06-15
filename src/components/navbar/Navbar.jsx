15d191ab3a2acf39186c7bb7573f20d79e58acf1
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-15 14:27:36 +0200

    Interactivity cart icon - not fully fixed yet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,16 +2,22 @@ import "./navbar.scss";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import { FaShoppingCart } from 'react-icons/fa'; // carrito de react-icons
 
 
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isCartActive, setIsCartActive] = useState(false);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     }
+
+    const handleCartActive = () => {
+        setIsCartActive(!isCartActive);
+    };
 
     return (
         <header className="nav-wrapper"> 
@@ -29,10 +35,12 @@ const Navbar = () => {
                 <span class="iconos-nav">
                     <img src="/assets/images/icons/Search.svg"  />
                     <img src="/assets/images/icons/Help.svg"  />
-                    <NavLink to="/cart" ><img className="cart-icon" src="/assets/images/icons/Cart.svg" /></NavLink> 
+                    <NavLink to="/cart" >
+                    <FaShoppingCart className={`cart-icon ${isCartActive ? 'active' : ''}`} 
+                    onClick={handleCartActive} /> </NavLink> 
                     <img src="/assets/images/icons/Avatar.svg" />
                     <div className="navbar-toggle" onClick={toggleMenu}>
-                    <label for="menu" className="menu-hamburgesa"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="25px" fill="#1a73e8"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+                        <label for="menu" className="menu-hamburgesa"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="25px" fill="#1a73e8"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
                         </label>
                         <div className={`bar ${isOpen ? 'open' : ''}`}></div>
                         <div className={`bar ${isOpen ? 'open' : ''}`}></div>
